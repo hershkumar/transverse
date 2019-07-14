d447415dbb416aefa0ec45355fdfc7d24f93d313
Unknown <hershkumar13@gmail.com>
2019-07-14 18:25:01 -0400

    add display/nickname change requests

--- transverse.js
+++ transverse.js
@@ -26,34 +26,34 @@ http.listen(port,'0.0.0.0', function(){
 // what to do when a client connects to the server
 io.sockets.on('connection', function(socket){
     var currentRoom;
+
+    socket.username = "Anonymous";
+    var clientIp = socket.request.connection.remoteAddress;
     // we expect the client to send the room name that they want to connect to
     socket.on('room', function(room){
         socket.join(room);
         currentRoom = room
         console.log("User joined room " + room)
-    });
-
+    }); 
     // What to do when a client sends a message to their room
     socket.on('chat', function(msg){
+        // send this message to all users in the room
+        socket.to(currentRoom).emit('chat', msg);
+        //log the message
+        console.log("\""+msg +"\"" + " in room "+ currentRoom);
+    });
+
+    socket.on('nameChange', function(newName){
+        socket.username = newName;
+    });
+    
+    socket.on('disconnect', function(){
+        var msg  = "User has disconnected from room " + currentRoom;
+        console.log(msg);
         socket.to(currentRoom).emit('chat', msg);
     });
 
-    // What to do when a client sends a command to their room
-    socket.on('command', function(command){
-
-    });
-
-    socket.on('disconnect', function(){
-        console.log("user has disconnected from room " + currentRoom);
-    });
-
 });
-
-
-
-
-
-
 
 // gets the timestamp in date time format
 function getTimeStamp() {
@@ -69,3 +69,9 @@ function getTimeStamp() {
             ? ("0" + now.getSeconds())
             : (now.getSeconds())));
 }
+
+
+
+
+
+
